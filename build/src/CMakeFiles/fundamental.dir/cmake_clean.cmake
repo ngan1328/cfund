be839abb0a74aaa98c93606c66d50efbfe923031
Cong Thanh <vcthanh20@gmail.com>
2024-02-27 13:15:49 +0700

    init base

---
+++ build/src/CMakeFiles/fundamental.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fundamental.dir/main.cpp.obj"
+  "CMakeFiles/fundamental.dir/main.cpp.obj.d"
+  "fundamental.exe"
+  "fundamental.exe.manifest"
+  "fundamental.pdb"
+  "libfundamental.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fundamental.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
